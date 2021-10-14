faf5f60be688cb3e2aa2f71c89eca1156679e631
Denis <d.zakorko@dashfight.com>
2021-10-14 16:33:45 +0300

    [telegram] Implemented scheduled message

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -3,9 +3,10 @@ import { TelegramService } from './telegram.service';
 import { TelegramController } from './telegram.controller';
 import { TelegrafModule } from "nestjs-telegraf";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ScheduleModule } from "@nestjs/schedule";
 
 @Module({
-  imports: [TelegrafModule.forRootAsync({
+  imports: [ScheduleModule.forRoot(),TelegrafModule.forRootAsync({
     imports: [ConfigModule],
     useFactory: async (configService: ConfigService) => ({
       token: configService.get('TELEGRAM_BOT_TOKEN')
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from "@nestjs/common";
+import {  Start, Update } from "nestjs-telegraf";
+import { Context } from "telegraf";
+import { SchedulerRegistry } from "@nestjs/schedule";
+import { CronJob } from "cron";
 
 @Injectable()
-export class TelegramService {}
+@Update()
+export class TelegramService {
+  private readonly logger = new Logger('Telegram');
+  constructor(private schedulerRegistry: SchedulerRegistry) {
+  }
+
+  @Start()
+  async sendScheduled(ctx: Context): Promise<void> {
+    const user = ctx.from
+    const message = `Шо не нравиця?`
+    const jobName = `MessageFor${user.username}`
+    const job = new CronJob(` 00 12 * * 0-6`,  async () => {
+      await ctx.reply(message)
+    },  async () => {
+      this.logger.log(`Sent scheduled message at ${job.nextDate()}`)
+    }, undefined, 'Europe/Moscow');
+    const exists = this.schedulerRegistry.doesExists('cron', jobName)
+    await ctx.reply(message)
+    if (exists) {
+      this.schedulerRegistry.deleteCronJob(jobName);
+    }
+      this.schedulerRegistry.addCronJob(jobName, job);
+      job.start();
+      this.logger.log(`Scheduled message at ${job.nextDate()}`)
+      return
+  }
+}
